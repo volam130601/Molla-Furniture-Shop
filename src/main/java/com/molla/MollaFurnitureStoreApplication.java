44559e787296deb939db9dfafda74718796db6ff
Admin <volam130601@gmail.com>
2022-10-03 23:20:51 +0700

    Init code

---
+++ src/main/java/com/molla/MollaFurnitureStoreApplication.java
@@ -0,0 +1,13 @@
+package com.molla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MollaFurnitureStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MollaFurnitureStoreApplication.class, args);
+    }
+
+}
